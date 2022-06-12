9eb0452ffd82f2f18dea573e4118a7853531f175
Work-Game <willydgeso@gmail.com>
2022-06-11 23:12:27 -0500

    implementacion de la clase main, loop, frame, update y draw, creacion de la clase constantes en donde se pondran las constantes del proyecto.

---
+++ src/Constantes.java
@@ -0,0 +1,10 @@
+public class Constantes {
+    //Frame------------------------------------------------------------------------------------------------------------
+    public static final int WIDTH = 800;
+    public static final int HEIGHT = 550;
+
+    //FPS--------------------------------------------------------------------------------------------------------------
+    public static final int NANOTIME = 1000000000;
+    private static final byte FPS = 72;
+    public static final double TIME = NANOTIME / FPS;
+}
---
+++ src/Main.java
@@ -0,0 +1,144 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.image.BufferStrategy;
+
+public class Main extends JFrame implements Runnable{
+    private Thread loop;
+    private boolean running;
+    private Canvas canvas;
+    private byte cfpd, fps;
+    private int caps, aps, runTime;
+
+    private long preStart;
+    private long timer;
+
+    public Main(){
+        initComponent();
+        this.setSize(Constantes.WIDTH, Constantes.HEIGHT);
+        this.setLocationRelativeTo(null);
+
+        clouseingComant();
+
+        canvas.requestFocus();
+    }
+
+    private void initComponent(){
+        preStart = System.nanoTime();
+        timer = System.nanoTime();
+
+        canvas = new Canvas();
+        canvas.setBackground(Color.BLACK);
+        this.getContentPane().add(canvas, BorderLayout.CENTER);
+
+        running = false;
+
+        fps = 0;
+        aps = 0;
+        cfpd = 0;
+        caps = 0;
+        runTime = 0;
+
+        this.setResizable(true);
+        this.setVisible(true);
+        this.setTitle("AstroShip");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+    }
+
+    public void start(){
+        if (running) return;
+        running = true;
+        loop = new Thread(this, "main loop");
+        loop.start();
+    }
+
+    private void stop(){
+        try {
+            running = false;
+            loop.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void clouseingComant(){
+        this.addWindowListener(new WindowAdapter() {
+            /**
+             * Invoked when a window is in the process of being closed.
+             * The close operation can be overridden at this point.
+             *
+             * @param e
+             */
+            @Override
+            public void windowClosing(WindowEvent e) {
+                super.windowClosing(e);
+                stop();
+            }
+        });
+    }
+
+    private void update(int dt){
+
+    }
+
+    private void draw(){
+        BufferStrategy bs = canvas.getBufferStrategy();
+        if(bs == null){
+            canvas.createBufferStrategy(2);
+            return;
+        }
+        Graphics g = bs.getDrawGraphics();
+        g.clearRect(0,0,Constantes.WIDTH,Constantes.HEIGHT);
+        g.setColor(Color.WHITE);
+        //start draw whatever------------------------------------------------------------------------------------------
+        g.drawString("FPS: "+fps+", APS: "+ aps +", Run time: "+runTime+"s.", 10, 15);
+        //end draw-----------------------------------------------------------------------------------------------------
+        g.dispose();
+        bs.show();
+        cfpd++;
+    }
+
+    /**
+     * When an object implementing interface {@code Runnable} is used
+     * to create a thread, starting the thread causes the object's
+     * {@code run} method to be called in that separately executing
+     * thread.
+     * <p>
+     * The general contract of the method {@code run} is that it may
+     * take any action whatsoever.
+     *
+     * @see Thread#run()
+     */
+    @Override
+    public void run() {
+        int seconds = 0;
+        double delta = 0;
+        //long start;
+        while (running){
+            long start = System.nanoTime();
+            delta += (start - preStart) / Constantes.TIME;
+            preStart = start;
+            while (delta >= 1){
+                update(seconds);
+                draw();
+                delta--;
+            }
+            caps++;
+            if(System.nanoTime() - timer > Constantes.NANOTIME){
+                timer += Constantes.NANOTIME;
+                seconds++;
+                runTime = seconds;
+                fps = cfpd;
+                aps = caps;
+                cfpd = 0;
+                caps = 0;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new Main().start();
+    }
+}
